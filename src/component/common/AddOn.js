5f39782c61b6207a737e8fd8397813e297e67809
Your Name <you@example.com>
2024-07-20 20:04:41 +0500

    slider added

--- src/component/common/AddOn.js
+++ src/component/common/AddOn.js
@@ -7,67 +7,93 @@ function AddOn() {
       name: "Pink Gypso Beauty Arrangement",
       price: "AED 249",
       imageUrl: "/flower1.jpg",
+      imageUrl2: "/floral-basket.jpg",
+      imageUrl3: "/red-roses.jpg",
     },
 
     {
       name: "Mixed Floral Basket",
       price: "AED 319",
       imageUrl: "/floral-basket.jpg",
+      imageUrl2:"/red-roses.jpg",
+      imageUrl3: "/flower1.jpg",
     },
     {
       name: "Pink Gypso Beauty Arrangement",
       price: "AED 249",
       imageUrl: "/flower1.jpg",
+      imageUrl2: "/floral-basket.jpg",
+      imageUrl3: "/red-roses.jpg",
     },
 
     {
       name: "Mixed Floral Basket",
       price: "AED 319",
       imageUrl: "/floral-basket.jpg",
+      imageUrl2: "/red-roses.jpg",
+      imageUrl3: "/flower1.jpg",
     },
     {
       name: "Pink Gypso Beauty Arrangement",
       price: "AED 249",
       imageUrl: "/flower1.jpg",
+      imageUrl2: "/floral-basket.jpg",
+      imageUrl3: "/red-roses.jpg",
     },
 
     {
       name: "Mixed Floral Basket",
       price: "AED 319",
       imageUrl: "/floral-basket.jpg",
+      imageUrl2: "/red-roses.jpg",
+      imageUrl3: "/flower1.jpg",
     },
     {
       name: "Pink Gypso Beauty Arrangement",
       price: "AED 249",
       imageUrl: "/flower1.jpg",
+      imageUrl2: "/floral-basket.jpg",
+      imageUrl3: "/red-roses.jpg",
     },
 
     {
       name: "Mixed Floral Basket",
       price: "AED 319",
       imageUrl: "/floral-basket.jpg",
+      imageUrl2: "/red-roses.jpg",
+      imageUrl3: "/flower1.jpg",
     },
     {
       name: "Pink Gypso Beauty Arrangement",
       price: "AED 249",
       imageUrl: "/flower1.jpg",
+      imageUrl2: "/floral-basket.jpg",
+      imageUrl3: "/red-roses.jpg",
     },
 
     {
       name: "Mixed Floral Basket",
       price: "AED 319",
       imageUrl: "/floral-basket.jpg",
+      imageUrl2:"/red-roses.jpg",
+      imageUrl3: "/flower1.jpg",
     },
     {
       name: "Pink Gypso Beauty Arrangement",
       price: "AED 249",
       imageUrl: "/flower1.jpg",
+      imageUrl2: "/floral-basket.jpg",
+      imageUrl3: "/red-roses.jpg",
+
+
     },
 
     {
       name: "Mixed Floral Basket",
       price: "AED 319",
       imageUrl: "/floral-basket.jpg",
+      imageUrl2: "/red-roses.jpg",
+      imageUrl3: "/flower1.jpg",
     },
   ];
   return (
@@ -79,6 +105,9 @@ function AddOn() {
             name={product.name}
             price={product.price}
             imageUrl={product.imageUrl}
+            imageUrl2={product.imageUrl2}
+            imageUrl3={product.imageUrl3}
+
           />
         ))}{" "}
       </div>
